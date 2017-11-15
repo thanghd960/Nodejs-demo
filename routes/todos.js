0dbd1598edc0ea29ddfb0bc633018be40c90e2f4
thanghd960 <huadaithang@gmail.com>
2017-11-15 14:33:10 +0700

    add fronted - using angular2

--- routes/todos.js
+++ routes/todos.js
@@ -24,15 +24,51 @@ router.get('/todo/:id', function(req, res, next){
     });
 });
 
-// Save Todo
-router.get('/todo', function(req, res, next){
+// Delete todo
+router.delete('/todo/:id', function(req, res, next){
+    // res.send('Todos');
+    db.todos.remove({_id: mongojs.ObjectId(req.params.id)},function(err, todo){
+        if(err){
+            res.send(err);
+        }
+        res.json(todo)
+    });
+});
+
+// Update todo
+router.put('/todo/:id', function(req, res, next){
     // res.send('Todos');
     var todo = req.body;
-    if(!todo.title || (todo.status + '')){
+    var updTodo = {};
+    if(todo.status){
+        updTodo.status = todo.status;
+    }
+    if(todo.title){
+        updTodo.title = todo.title;
+    }
+    if(!updTodo){
         res.status(400);
         res.json({
             "error":"Bad data"
-        })
+        });
+    }else{
+        db.todos.update({_id: mongojs.ObjectId(req.params.id)},updTodo,{},function(err, todo){
+            if(err){
+                res.send(err);
+            }
+            res.json(todo)
+        });
+    }    
+});
+
+// Save Todo
+router.post('/todo', function(req, res, next){
+    var todo = req.body;
+    if(!todo.title || !(todo.status + '')){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
     } else {
         db.todos.save(todo, function(err, todo){
             if(err){
@@ -41,7 +77,6 @@ router.get('/todo', function(req, res, next){
             res.json(todo);
         });
     }
-    
 });
 
 
